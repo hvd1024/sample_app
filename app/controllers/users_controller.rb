c2c469aac65cacfc47d7aa46423aa6fc0d644f74
hvd <i.hvd96@gmail.com>
2018-09-18 09:36:10 +0700

    Finish user signup
    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Chào mừng bạn đến thế giới của chúng tôi !!!"
       redirect_to @user
     else
